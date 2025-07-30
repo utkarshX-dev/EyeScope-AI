8df3df039c00041f765522998aa8ac48bfcb1d04
utkarshBhandari <utkarshX-dev@users.noreply.github.com>
2025-07-30 19:57:21 +0530

    deployment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import '../dist/output.css'
+import "./output.css";
+// import "../dist/output.css"; 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
